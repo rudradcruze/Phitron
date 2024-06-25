8d5b57ba3ba505295b77198cdc5a1c1c2dd621af
rudradcruze <francisrudra@gmail.com>
2024-06-26 00:32:00 +0600

    dynamic object

--- Semester 1/Introduction-to-C++-for-DSA/Modules/Module 6/dynamic_object.cpp
+++ Semester 1/Introduction-to-C++-for-DSA/Modules/Module 6/dynamic_object.cpp
@@ -9,12 +9,33 @@ using namespace std;
 
 class Person
 {
-
+    public:
+        string name;
+        int age;
+    
+        Person(string name, int age)
+        {
+            this->name = name;
+            this->age = age;
+        }
 };
 
 int main()
 {
     
+    Person* rudra = new Person("Francis Rudra D Cruze", 24);
+    Person* john = new Person("John Doe", 30);
+
+    // rudra=john;
+    // delete john;
+
+    // rudra->name = john->name;
+    // rudra->age = john->age;
+
+    *rudra = *john;
+    delete john;
+
+    cout << rudra->name << " " << rudra->age << endl;
 
     return 0;
 }
